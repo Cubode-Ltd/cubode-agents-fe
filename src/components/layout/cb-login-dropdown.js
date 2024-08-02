f5552936a8076ccf4cecc9f176a1757bb77364bd
cubode <david.garcia@cubode.com>
2024-08-02 15:26:58 +0200

    profile text

--- src/components/layout/cb-login-dropdown.js
+++ src/components/layout/cb-login-dropdown.js
@@ -43,7 +43,7 @@ template.innerHTML = `
         <a href="#" class="avatar cursor-pointer" title="profile">
           <svg xmlns="http://www.w3.org/2000/svg" class="rounded-full border-2 border-white bg-gray-400 w-7 h-7" viewBox="0 0 16 16">
               <path d="m 8 1 c -1.65625 0 -3 1.34375 -3 3 s 1.34375 3 3 3 s 3 -1.34375 3 -3 s -1.34375 -3 -3 -3 z m -1.5 7 c -2.492188 0 -4.5 2.007812 -4.5 4.5 v 0.5 c 0 1.109375 0.890625 2 2 2 h 8 c 1.109375 0 2 -0.890625 2 -2 v -0.5 c 0 -2.492188 -2.007812 -4.5 -4.5 -4.5 z m 0 0" fill="#2e3436"/>
-              <text x="50%" y="55%" text-anchor="middle" dy=".3em" font-size="8" fill="white" font-family="Arial, sans-serif"></text>
+              <text x="50%" y="65%" text-anchor="middle" dy=".3em" font-size="8" font-weight="bold" fill="white" font-family="Arial, sans-serif"></text>
           </svg>
         </a>
       </div>
